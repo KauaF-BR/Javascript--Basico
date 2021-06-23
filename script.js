2b9cf1b3f30223bbc52f4ce631625b10c25b763d
KauaF-BR <Nascimento200408@gmail.com>
2021-06-23 08:14:42 -0300

    Comparative Operators / Operadores comparativos
    //    pt-br                |     en
    // ==  igual a             | equal to
    // === valor igual e igual | equal and equal value
    // ! = não é igual         | not equal
    // > maior que             | greater than
    // < menor que             | less than
    // >= maior que ou igual a | greater than or equal to
    // <= menor que ou igual a | less than or equal to

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// =   x = y    x = y
-// +-  x += y   x = x + y
-// -=  x -= y   x = x - y
-// *=  x *= y   x = x * y
-// /=  x /= y   x = x / y
-//
+//    pt-br                |     en
+// ==  igual a             | equal to
+// === valor igual e igual | equal and equal value
+// ! = não é igual         | not equal
+// > maior que             | greater than
+// < menor que             | less than
+// >= maior que ou igual a | greater than or equal to
+// <= menor que ou igual a | less than or equal to
