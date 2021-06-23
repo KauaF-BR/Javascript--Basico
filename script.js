f1ff896f7335facbfa7fae1d3460222f4be837ac
KauaF-BR <Nascimento200408@gmail.com>
2021-06-23 20:34:32 -0300

    Logical perators

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//    pt-br                |     en
-// ==  igual a             | equal to
-// === valor igual e igual | equal and equal value
-// ! = não é igual         | not equal
-// > maior que             | greater than
-// < menor que             | less than
-// >= maior que ou igual a | greater than or equal to
-// <= menor que ou igual a | less than or equal to
+// && e
+// || ou
+// Negativo
