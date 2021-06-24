733ca36795fd73741f11ba360434c7f9886ea345
KauaF-BR <Nascimento200408@gmail.com>
2021-06-24 09:25:02 -0300

    learning the meaning of the string
    var numero = 8
    var str ="Valor qualquer " + numero
    console.log(numero)

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// && e
-// || ou
-// Negativo
 
-
-var idade = 35
-
-var maior20 = idade >= 20
-var menor20 = !menor20
-
-
-console.log("Maior que vinte", maior20)
-console.log("Menor que vinte", menor20)
+var numero = 8
+var str ="Valor qualquer " + numero
+console.log(numero)
