380c3f144ea4ebc9f054dd9d73fca815fba25cfe
KauaF-BR <Nascimento200408@gmail.com>
2021-06-24 08:46:12 -0300

    Exemplo &&
    var idade = 22
    var maior20 = idade >= 20
    var menor30 = idade <= 30
    
    console.log("Idade", idade)
    console.log("Maior que vinte", maior20)
    console.log("Menor que vinte", menor30)
    
    var entre = maior20 && menor30
    
    console.log("Entre 20 e 30", entre)

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
 // Negativo
 
 
-var a = true
-var b = false
+var idade = 22
+var maior20 = idade >= 20
+var menor30 = idade <= 30
 
-var c = a && b
 
-console.log(c);
+
+
+console.log("Idade", idade)
+console.log("Maior que vinte", maior20)
+console.log("Menor que vinte", menor30)
+
+var entre = maior20 && menor30
+
+
+console.log("Entre 20 e 30", entre)
