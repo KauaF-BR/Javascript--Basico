030973fc5c7eed9d3dcd853c725a84b78cf1d991
KauaF-BR <Nascimento200408@gmail.com>
2021-06-24 08:55:57 -0300

    Exemplo ||
    // && e
    // || ou
    // Negativo
    
    var idade = 9
    
    var menor10 = idade <= 10
    var maior65 = idade >= 65
    
    var gratuidade = menor10 || maior65
    
    console.log("Idade", idade)
    
    console.log("Menor que dez", menor10)
    
    console.log("Maior que sessenta e cinco", maior65)
    
    console.log("Tem direito a gratuidade", gratuidade)

--- script.js
+++ script.js
@@ -3,18 +3,17 @@
 // Negativo
 
 
-var idade = 22
-var maior20 = idade >= 20
-var menor30 = idade <= 30
+var idade = 9
 
+var menor10 = idade <= 10
+var maior65 = idade >= 65
 
-
+var gratuidade = menor10 || maior65
 
 console.log("Idade", idade)
-console.log("Maior que vinte", maior20)
-console.log("Menor que vinte", menor30)
 
-var entre = maior20 && menor30
+console.log("Menor que dez", menor10)
 
+console.log("Maior que sessenta e cinco", maior65)
 
-console.log("Entre 20 e 30", entre)
+console.log("Tem direito a gratuidade", gratuidade)
