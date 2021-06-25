a057b2b57ce878ec9183cbc4334846a230914ccd
KauaF-BR <Nascimento200408@gmail.com>
2021-06-25 10:11:30 -0300

    Example one

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var idade = 18
 
-if (idade >= 18){
+if (idade >= 35){
     console.log("pode")
     console.log("Qual o seu pedido?")
 }
+
+else if ( idade >= 18) {
+    console.log("pode")
+    console.log("Mostre a identidade")
+}
+
 else{
     console.log("não pode")
     console.log("Volte futuramente")
