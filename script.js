44505c7df412fd72c04641b50fc0c05ad5478674
KauaF-BR <Nascimento200408@gmail.com>
2021-06-25 19:52:18 -0300

    Example  four

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-var idade = 15
+var idade = prompt("Qual sua idade?");
 
-if (idade < 18 || idade > 70){
-    console.log("pode")
-    console.log("Qual o seu pedido?")
-}
+if (idade >= 75) {
+  alert("bem vindo senhor");
+  alert("Recomendamos beber com moderação");
+  alert("em que posso te ajudar?");
+} 
 
 
+else if (idade >= 18) {
+  alert("bem vindo a loja");
+  alert("em que posso te ajudar?");
+} 
+
+else if (idade < 18) {
+  alert("você nao tem idade pra acessar nossa loja");
+  alert("ficaremos felizes em te atender quando você for maior de idade");
+} 
 
 
-else{
-    console.log("não pode")
-    console.log("Volte futuramente")
-}
+else alert("o campo nao foi preenchido corretamente");
