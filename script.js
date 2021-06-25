b48480c8e2a318fe3dd1dfd0a725598689b310a6
KauaF-BR <Nascimento200408@gmail.com>
2021-06-25 10:14:07 -0300

    Example two with "&&"

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var idade = 18
+var idade = 15
 
-if (idade >= 35){
+if (idade >= 18 && idade <= 70){
     console.log("pode")
     console.log("Qual o seu pedido?")
 }
 
-else if ( idade >= 18) {
-    console.log("pode")
-    console.log("Mostre a identidade")
-}
+
+
 
 else{
     console.log("não pode")
