926c1a1bae1539d428b36c12ea2a5d6a17e680ab
KauaF-BR <Nascimento200408@gmail.com>
2021-06-25 19:57:46 -0300

    Operador Ternário

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-var idade = prompt("Qual sua idade?");
-
-if (idade >= 75) {
-  alert("bem vindo senhor");
-  alert("Recomendamos beber com moderação");
-  alert("em que posso te ajudar?");
-} 
+var idade = 17
 
 
-else if (idade >= 18) {
-  alert("bem vindo a loja");
-  alert("em que posso te ajudar?");
-} 
-
-else if (idade < 18) {
-  alert("você nao tem idade pra acessar nossa loja");
-  alert("ficaremos felizes em te atender quando você for maior de idade");
-} 
-
-
-else alert("o campo nao foi preenchido corretamente");
+idade >= 18 ? console.log("Pode") : console.log("Não pode")
