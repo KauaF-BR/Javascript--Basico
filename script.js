2b0d16ab320a0c5a48094f38e984fb2e5ffae118
KauaF-BR <Nascimento200408@gmail.com>
2021-07-03 10:55:25 -0300

    Condicionais - Switch EXAMPLE

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var idade = 17
+var nota1 = 5.0
+var nota2 = 5.8
 
 
-idade >= 18 ? console.log("Pode") : console.log("Não pode")
+var media = (nota1 + nota2) / 2
+
+
+if (media >= 7) {
+
+    console.log("Você passou com média " + media)
+}
+
+else{
+    console.log("Você reprovou com média " + media + ", não foi o suficiente para passar")
+}
